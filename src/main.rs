ff4802b3e47221ad65be3cdc07ec596997a52850
Lawand Othman <lwnd@pm.me>
2022-09-20 00:19:26 +0100

    Feat: get secret word

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{fs, io};
+
+use rand::seq::IteratorRandom;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_word = match get_secret_word() {
+        Ok(secret_word) => secret_word,
+        Err(e) => panic!("Problem opening file: {}", e),
+    };
+    println!("The random word is {} ", secret_word);
+    println!("The secret word's length is {}", secret_word.len());
+}
+
+fn get_secret_word() -> Result<String, io::Error> {
+    let file = fs::read_to_string("words.txt")?;
+    let mut rng = rand::thread_rng();
+    let secret_word = file.lines().choose(&mut rng).unwrap().to_string();
+    Ok(secret_word)
 }
