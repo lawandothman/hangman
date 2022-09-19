48308e64d04981d96db05fdbdaf812297ff4d579
Lawand Othman <lwnd@pm.me>
2022-09-20 00:29:18 +0100

    Feat: hide secret word

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ fn main() {
         Ok(secret_word) => secret_word,
         Err(e) => panic!("Problem opening file: {}", e),
     };
+
+    let hidden_secret_word = hide_secret_word(&secret_word);
     println!("The random word is {} ", secret_word);
     println!("The secret word's length is {}", secret_word.len());
+    print!("{}", hidden_secret_word);
 }
 
 fn get_secret_word() -> Result<String, io::Error> {
@@ -16,4 +19,14 @@ fn get_secret_word() -> Result<String, io::Error> {
     let mut rng = rand::thread_rng();
     let secret_word = file.lines().choose(&mut rng).unwrap().to_string();
     Ok(secret_word)
+}
+
+fn hide_secret_word(input: &str) -> String {
+    let mut result = String::new();
+    for (_, c) in input.chars().enumerate() {
+        result.push(if c == ' ' { c } else { '_' });
+        result.push(' ');
+    }
+    result.push('\n');
+    result
 }
